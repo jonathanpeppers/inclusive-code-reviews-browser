812ee87b39b9548afe6c18c32c34d61eb2dd6d9f
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-03-04 10:50:11 -0600

    Only replace backticks

--- src-packed/textAnalytics.js
+++ src-packed/textAnalytics.js
@@ -80,18 +80,17 @@ function removeCodeBlocks (text) {
     return result;
 }
 
-// Replace backticks and single quotes for double quotes
-// since backticks and single quotes make sentiment more negative
-function replaceQuotes (text) {
+// Replace backticks for double quotes, since backticks make sentiment more negative
+// See textAnalytics.js 'Yield Example' test
+function replaceBackticks (text) {
     var result = text.replace(/`/g, "\"");
-    result = result.replace(/\'/g, "\"");
     return result;
 }
 
 export function preprocessText (text) {
     var result = removeCodeBlocks (text);
     result = removeImageTags (result);
-    result = replaceQuotes (result);
+    result = replaceBackticks (result);
     return result;
 }
 
