dd61fe956dd4335253619c1338f6b7992d76e4c2
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-03-04 17:11:04 +0100

    Add support for reporting an acceptable sentence. Fixes #83. (#93)
    Add a button to report that a sentence should not be flagged. For now this
    just creates a new GitHub issue with the title and body filled in with the
    sentence in question.
    
    Fixes https://github.com/jonathanpeppers/inclusive-code-comments/issues/83.
    
    * Use the existing 'Report an issue' button, but change the message.
    
    Also improve the GitHub issue:
    
    * Add labels.
    * Add more information about the flagged word or sentence.
    
    Co-authored-by: Jonathan Peppers <jonathan.peppers@gmail.com>

--- src/content/errorCard.js
+++ src/content/errorCard.js
@@ -160,7 +160,18 @@ class ErrorCard {
     }
     _showGithubIssues(e) {
         e.stopImmediatePropagation();
-        window.open("https://github.com/jonathanpeppers/inclusive-code-comments/issues/new");
+        let title = "Report sentence to improve heuristics";
+        let body = "The following word or sentence is acceptable:\n\n    " + this._error.originalPhrase + "\n\n";
+        body += "Additional info:\n\n";
+        body += "* Description: " + this._error.description + "\n";
+        body += "* Context phrase: " + this._error.contextPhrase + "\n";
+        body += "* isPartialValidation: " + this._error.isPartialValidation + "\n";
+        body += "* isPicky: " + this._error.isPicky + "\n";
+        body += "* isPunctuationError: " + this._error.isPunctuationError + "\n";
+        body += "* isSpellingError: " + this._error.isSpellingError + "\n";
+        body += "* isStyleError: " + this._error.isStyleError + "\n";
+        body += "* language: " + this._error.language.code + " (" + this._error.language.name + ")" + "\n";
+        window.open("https://github.com/jonathanpeppers/inclusive-code-comments/issues/new?labels=" + encodeURIComponent("enhancement,heuristics") + "&title=" + encodeURIComponent(title) + "&body=" + encodeURIComponent(body));
     }
     _onBadgeClicked(e) {
         e.stopImmediatePropagation();
