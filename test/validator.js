e171d1348b667b4c27e17dda4b1fc3578b486ed1
TJ Lambert <tjlambert@microsoft.com>
2022-12-02 09:28:36 -0600

    add awaits and another test to catch negative sentiment before brevity

--- test/validator.js
+++ test/validator.js
@@ -71,20 +71,29 @@ describe('validator', () => {
     it('Brief text suggestion', async () => {
         var text = "Ok?";
         var matches = [];
-        client.getMatches(ort, text, matches);
+        await client.getMatches(ort, text, matches);
         expect(matches.length).to.be.equal(1);
         expect(matches[0].length).to.be.equal(text.length);
+        expect(matches[0].shortMessage).to.be.equal("Comment is brief");
     });
     
     it('Brief text no suggestion', async () => {
         var matches = [];
-        client.getMatches(ort, "Thank you", matches);
+        await client.getMatches(ort, "Thank you", matches);
         expect(matches.length).to.be.equal(0);
     });
 
     it('Brief text in ignore list no suggestion', async () => {
         var matches = [];
-        client.getMatches(ort, "Fixes #77", matches);
+        await client.getMatches(ort, "Fixes #77", matches);
         expect(matches.length).to.be.equal(0);
+    });
+
+    it('Brief text but catches negative connotation first', async () => {
+        var matches = [];
+        await client.getMatches(ort, "Sucks", matches);
+        expect(matches.length).to.be.equal(2);
+        expect(matches[0].shortMessage).to.be.equal("Negative sentiment");
+        expect(matches[1].shortMessage).to.be.equal("Comment is brief");
     });
 });
