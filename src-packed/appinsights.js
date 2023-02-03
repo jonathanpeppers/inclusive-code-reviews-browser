f4338c32db23c0786c4f143a53e59f63d5ba8807
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-02-03 14:48:18 -0600

    Use `globalThis` over `window` where appropriate (#196)
    Trying to slowly port changes from `manifest-v3` to `main`.
    
    This might help identify a few things that aren't working yet in `manifest-v3`.

--- src-packed/appinsights.js
+++ src-packed/appinsights.js
@@ -65,6 +65,7 @@ export function trackEvent(name, customDimensions) {
 }
 
 // For use inside the extension (which isn't using webpack)
-// The best I came up with for now is to add these functions to window.
-window.aiTrackPageView = trackPageView;
-window.aiTrackEvent = trackEvent;
+// The best I came up with for now is to add these functions to globalThis.
+// https://developer.mozilla.org/en-US/docs/Glossary/Global_object
+globalThis.aiTrackPageView = trackPageView;
+globalThis.aiTrackEvent = trackEvent;
