4259ad5cf570e7186bc7dbb2683073e30298106a
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-05-05 09:17:56 -0500

    Bump to version 3.0.2

--- src-packed/appinsights.js
+++ src-packed/appinsights.js
@@ -19,7 +19,7 @@ if (!isTests) {
 }
 
 export function telemetryInitializer (envelope) {
-    envelope.tags["ai.application.ver"] = '3.0.1';
+    envelope.tags["ai.application.ver"] = '3.0.2';
 
     // We don't want to report full URLs
     if (envelope.baseData.uri) {
