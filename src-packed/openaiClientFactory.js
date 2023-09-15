c2b0c3ae223787f2e8356dde7fb85197d648d417
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-09-15 14:38:51 -0500

    openaiClientFactory cleanup

--- src-packed/openaiClientFactory.js
+++ src-packed/openaiClientFactory.js
@@ -1,65 +1,23 @@
-const { Configuration, OpenAI } = require("openai");
+const { OpenAI } = require("openai");
 
-const configObjectLocalStorageName = "icr-openai-config";
-
-export function setAzureManagedConfig(endpoint, key) {
-  let config = {
-    azureEndpoint: endpoint,
-    azureKey: key
-  };
-
-  window.localStorage.setItem(configObjectLocalStorageName, JSON.stringify(config));
-}
-
-export function setPublicOpenaiConfig(key) {
-  let config = {
-    openaiApiKey: key
-  };
-
-  window.localStorage.setItem(configObjectLocalStorageName, JSON.stringify(config));
-}
-
-export function clearOpenaiConfig() {
-  window.localStorage.removeItem(configObjectLocalStorageName);
-}
-
-export function getOpenaiClient() {
-  let serilizedConfig = window.localStorage.getItem(configObjectLocalStorageName);
-
-  if (!serilizedConfig) {
-    return null;
-  }
-
-  let parsedConfig = JSON.parse(serilizedConfig);
-
-  if (Object.hasOwn(parsedConfig, 'azureEndpoint') && Object.hasOwn(parsedConfig, 'azureKey')) {
-    if (!parsedConfig.azureEndpoint.endsWith("/")) {
-      parsedConfig.azureEndpoint = parsedConfig.azureEndpoint + "/";
+export function getOpenaiClient(apiKey, baseURL) {
+    var config = {
+        apiKey,
+        // NOTE: this warns inside our tests and the chrome extension, have no choice really
+        dangerouslyAllowBrowser: true
     }
-
-    var openai = new OpenAI({
-      apiKey: parsedConfig.azureKey,
-      baseURL: `${parsedConfig.azureEndpoint}openai/deployments/icrgpt-35-turbo-16k`,
-      defaultHeaders: { 
-        'api-key': parsedConfig.azureKey
-      },
-      defaultQuery:  {
-        // https://learn.microsoft.com/azure/cognitive-services/openai/reference#chat-completions
-        'api-version': '2023-03-15-preview'
-      },
-      // NOTE: this warns inside our tests and the chrome extension, have no choice really
-      dangerouslyAllowBrowser: true
-    });
-
-    return openai;
-  } else if (Object.hasOwn(parsedConfig, 'openaiApiKey')){
-    var openai = new OpenAI({
-      apiKey: parsedConfig.openaiApiKey,
-      // NOTE: this warns inside our tests and the chrome extension, have no choice really
-      dangerouslyAllowBrowser: true
-    });
-    return openai;
-  }
-
-  return null;
+    if (baseURL) {
+        if (!baseURL.endsWith("/")) {
+            baseURL += "/";
+        }
+        config.baseURL = baseURL;
+        config.defaultHeaders = { 
+            'api-key': apiKey
+        };
+        config.defaultQuery = {
+            // https://learn.microsoft.com/azure/cognitive-services/openai/reference#chat-completions
+            'api-version': '2023-03-15-preview'
+        };
+    }
+    return new OpenAI(config);
 } 
--- test/openaiClientFactoryTest.js
+++ test/openaiClientFactoryTest.js
@@ -1,113 +1,5 @@
-const { openai_key, public_openai_key } = require("../src-packed/secrets");
+const { openai_key } = require("../src-packed/secrets");
 var assert = require('assert');
-
-// Mock localStorage
-(function () {
-
-    function createStorage() {
-        let UNSET = Symbol();
-        let s = {};
-        let noopCallback = () => { };
-        let _itemInsertionCallback = noopCallback;
-
-        Object.defineProperty(s, 'setItem', {
-            get: () => {
-                return (k, v = UNSET) => {
-                    if (v === UNSET) {
-                        throw new TypeError(`Failed to execute 'setItem' on 'Storage': 2 arguments required, but only 1 present.`);
-                    }
-                    if (!s.hasOwnProperty(String(k))) {
-                        _itemInsertionCallback(s.length);
-                    }
-                    s[String(k)] = String(v);
-                };
-            }
-        });
-
-        Object.defineProperty(s, 'getItem', {
-            get: () => {
-                return k => {
-                    if (s.hasOwnProperty(String(k))) {
-                        return s[String(k)];
-                    } else {
-                        return null;
-                    }
-                };
-            }
-        });
-
-        Object.defineProperty(s, 'removeItem', {
-            get: () => {
-                return k => {
-                    if (s.hasOwnProperty(String(k))) {
-                        delete s[String(k)];
-                    }
-                };
-            }
-        });
-
-        Object.defineProperty(s, 'clear', {
-            get: () => {
-                return () => {
-                    for (let k in s) {
-                        delete s[String(k)];
-                    }
-                };
-            }
-        });
-
-        Object.defineProperty(s, 'length', {
-            get: () => {
-                return Object.keys(s).length;
-            }
-        });
-
-        Object.defineProperty(s, "key", {
-            value: k => {
-                let key = Object.keys(s)[String(k)];
-                return (!key) ? null : key;
-            },
-        });
-
-        Object.defineProperty(s, 'itemInsertionCallback', {
-            get: () => {
-                return _itemInsertionCallback;
-            },
-            set: v => {
-                if (!v || typeof v != 'function') {
-                    v = noopCallback;
-                }
-                _itemInsertionCallback = v;
-            }
-        });
-
-        return s;
-    }
-
-    const global = require("global")
-    const window = require("global/window")
-
-    Object.defineProperty(global, 'Storage', {
-        value: createStorage,
-    });
-    Object.defineProperty(window, 'Storage', {
-        value: createStorage,
-    });
-
-    Object.defineProperty(global, 'localStorage', {
-        value: createStorage(),
-    });
-    Object.defineProperty(window, 'localStorage', {
-        value: global.localStorage,
-    });
-
-    Object.defineProperty(global, 'sessionStorage', {
-        value: createStorage(),
-    });
-    Object.defineProperty(window, 'sessionStorage', {
-        value: global.sessionStorage,
-    });
-}());
 
 describe('openai client factory', () => {
     // Just return if the key is not set.
@@ -117,52 +9,15 @@ describe('openai client factory', () => {
         return;
     }
 
-    let azureEndpoint = "https://icropenaiservice.openai.azure.com/";
-
+    let endpoint = "https://icropenaiservice.openai.azure.com/openai/deployments/icrgpt-35-turbo-16k";
     const factory = require('../src-packed/openaiClientFactory');
 
-    it('would get empty', () => {
-        factory.clearOpenaiConfig();
-        var result = factory.getOpenaiClient();
-        assert.equal(result, undefined);
-    });
-
-    it('would store the azure config and get the instance', async () => {
-        factory.setAzureManagedConfig(azureEndpoint, openai_key);
-
-        var openai = factory.getOpenaiClient();
-        assert.notEqual(openai, undefined);
-
-        const response = await openai.chat.completions.create({
-            model: 'text-davinci-003',
-            messages:
-                [
-                    {
-                        "role": "user",
-                        "content": 'What is the meaning of life?'
-                    }
-                ],
-            // 0 accurate, 1 creative
-            temperature: 0.5
-        });
-        let result = response.choices[0].message.content;
-        expect(result.length).to.not.be.equal(0);
-
-        // clear would remove the stored config
-        factory.clearOpenaiConfig();
-
-        openai = factory.getOpenaiClient();
-        assert.equal(openai, undefined);
-    }).timeout(5000);
-
     it('basic comment rewrite', async () => {
-        factory.setAzureManagedConfig(azureEndpoint, openai_key);
-        var openai = factory.getOpenaiClient();
+        var openai = factory.getOpenaiClient(openai_key, endpoint);
         assert.notEqual(openai, undefined);
 
         let comment = "Remove this line of code. This is a wasted line of code.";
         const response = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo-16k',
             messages:
                 [
                     {
@@ -185,14 +40,12 @@ describe('openai client factory', () => {
     }).timeout(5000);
 
     it('basic comment rating', async () => {
-        factory.setAzureManagedConfig(azureEndpoint, openai_key);
-        var openai = factory.getOpenaiClient();
+        var openai = factory.getOpenaiClient(openai_key, endpoint);
         assert.notEqual(openai, undefined);
 
         let comment = "Remove this line of code. This is a wasted line of code.";
 
         const response = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
             messages:
                 [
                     {
