3b7845a3783f618db04a34c95894cc0358987ac5
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-12-15 09:49:19 -0600

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 console.log('service worker loaded');
 
-// These were the order of V2 background scripts
+// These were the order of V2 background.scripts
 try {
     importScripts("packed.js", "config/config.js", "common/browserDetector.js", "common/utils.js", "common/eventBus.js", "common/messages.js", "common/i18nManager.js", "common/extensioni18nManager.js", "common/languageManager.js", "common/storageController.js", "common/extensionStorageController.js", "common/environmentAdapter.js", "common/extensionEnvironmentAdapter.js", "common/extension-init.js", "common/tracker.js", "background/graphemeSplitter.js", "background/validator.js", "background/dictionarySync.js", "background/synonyms.js", "background/extension-main.js");
 } catch (e) {
