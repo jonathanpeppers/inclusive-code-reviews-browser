f038f2ae510f35ee54050bf23b83a7b7dbde22b4
wuweng <wuweng@microsoft.com>
2024-02-15 14:28:26 -0800

    add comment rewrite sample

--- test/openaiClientTest.js
+++ test/openaiClientTest.js
@@ -2,6 +2,7 @@ const timeout = 20000;
 const retries = 2;
 const endpoint = "https://app-rel.wus3.sample-dev.azgrafana-test.io/api/ChatCompletion";
 const endpointv4 = "https://app-rel.wus3.sample-dev.azgrafana-test.io/api/Gpt4ChatCompletion";
+const commentRewriteEndpoint = "https://app-rel.wus3.sample-dev.azgrafana-test.io/api/CommentRewrite";
 
 describe('openai client', () => {
     it('basic comment rewrite', async () => {
@@ -65,6 +66,37 @@ describe('openai client gpt4', () => {
         };
 
         const serverResponse = await fetch(endpointv4, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(request),
+        }).then(response => response.json());
+
+        let result = serverResponse.choices[0].message.content;
+
+        expect(result.length).to.not.be.equal(0);
+        expect(result).to.not.be.contains("This is a wasted line of code");
+    }).timeout(timeout).retries(retries);
+});
+
+
+describe('comment rewrite endpoint', () => {
+    it('check comment rewrite basic usage', async () => {
+        let comment = "Remove this line of code. This is a wasted line of code.";
+
+        let request = {
+            "comment": comment,
+            // 0 accurate, 1 creative
+            "temperature": 0.5,
+            // prevent the model from repeating itself without sacrificing quality of response
+            "frequencyPenalty": 1.0,
+            "maxTokens": 800,
+            "enableJsonMode": true,
+            "seed": 4567
+        };
+
+        const serverResponse = await fetch(commentRewriteEndpoint, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
