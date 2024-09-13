f9a01b6c4658d40d393c99f3937ccd0a228c3685
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-09-13 11:25:09 -0500

    Use CDN for .wasm binaries

--- _import.js
+++ _import.js
@@ -1,2 +1,7 @@
 import * as ort from 'onnxruntime-web';
 globalThis.ort = ort;
+
+// Use a CDN for the WebAssembly binaries, see:
+// * https://onnxruntime.ai/docs/tutorials/web/deploy.html#webassembly-binaries
+// * https://www.jsdelivr.com/
+ort.env.wasm.wasmPaths = 'https://cdn.jsdelivr.net/npm/onnxruntime-web@1.19.2/dist/';
--- src/background.js
+++ src/background.js
@@ -1,5 +1,12 @@
 console.log('service worker loaded');
 
+// HACK: workaround for onnxruntime-web
+// background.js:7  ReferenceError: document is not defined
+// at packed.js:2:623031
+// at packed.js:2:623265
+// at background.js:5:5
+document = { };
+
 // These were the order of V2 background.scripts
 try {
     importScripts("packed.js", "config/config.js", "common/browserDetector.js", "common/utils.js", "common/eventBus.js", "common/messages.js", "common/i18nManager.js", "common/extensioni18nManager.js", "common/languageManager.js", "common/storageController.js", "common/extensionStorageController.js", "common/environmentAdapter.js", "common/extensionEnvironmentAdapter.js", "common/extension-init.js", "common/tracker.js", "background/graphemeSplitter.js", "background/validator.js", "background/dictionarySync.js", "background/synonyms.js", "background/extension-main.js");
